079ffa260d6a6024b149a2ecdc24f6f58afb5864
Dominion <adebiyiquawiy2006@gmail.com>
2025-07-21 19:00:43 +0100

    alot of changes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
+const openNotification = () => {
+    const notification = document.getElementById('notification');
+    notification.classList.remove('hidden');
+    document.body.classList.add("overflow-hidden")
+  }
+const closeNotification = () => {
+    const notification = document.getElementById('notification');
+    notification.classList.add('hidden');
+    document.body.classList.remove("overflow-hidden")
+  }
 
 const toggleMobileSidebar = () => {
     const sidebar = document.getElementById('mobileSidebar');
     sidebar.classList.remove('-translate-x-full');
+    document.body.classList.add("overflow-hidden");
   }
 
 const closeSideBar = () => {
     const sidebar = document.getElementById('mobileSidebar');
     sidebar.classList.add('-translate-x-full');
+    document.body.classList.remove("overflow-hidden")
   }
 
 
@@ -79,3 +91,62 @@ carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 carousel.addEventListener("mouseup", dragStop);
 carousel.addEventListener("scroll", infiniteScroll);
+
+let aIsUp = true;
+const openExplore = () => {
+    const content = document.getElementById('explore-content');
+    const btn = document.getElementById('explore-drp');
+    aIsUp = !aIsUp;
+    content.classList.toggle('hidden');
+    if (aIsUp) {
+      btn.classList.remove("fa-chevron-up");
+      btn.classList.add("fa-chevron-down");
+    } else {
+      btn.classList.remove("fa-chevron-down");
+      btn.classList.add("fa-chevron-up");
+    }
+  }
+
+let bIsUp = true;
+const openAbout = () => {
+    const content = document.getElementById('about-content');
+    const btn = document.getElementById('about-drp');
+    bIsUp = !bIsUp;
+    content.classList.toggle('hidden');
+    if (bIsUp) {
+      btn.classList.remove("fa-chevron-up");
+      btn.classList.add("fa-chevron-down");
+    } else {
+      btn.classList.remove("fa-chevron-down");
+      btn.classList.add("fa-chevron-up");
+    }
+  }
+
+let CIsUp = true;
+const openResources = () => {
+    const content = document.getElementById('resources-content');
+    const btn = document.getElementById('resources-drp');
+    CIsUp = !CIsUp;
+    content.classList.toggle('hidden');
+    if (CIsUp) {
+      btn.classList.remove("fa-chevron-up");
+      btn.classList.add("fa-chevron-down");
+    } else {
+      btn.classList.remove("fa-chevron-down");
+      btn.classList.add("fa-chevron-up");
+    }
+  }
+let DIsUp = true;
+const openPc = () => {
+    const content = document.getElementById('pc-content');
+    const btn = document.getElementById('pc-drp');
+    DIsUp = !DIsUp;
+    content.classList.toggle('hidden');
+    if (DIsUp) {
+      btn.classList.remove("fa-chevron-up");
+      btn.classList.add("fa-chevron-down");
+    } else {
+      btn.classList.remove("fa-chevron-down");
+      btn.classList.add("fa-chevron-up");
+    }
+  }
